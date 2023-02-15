3c599ae75e386925edb77b380c643a9cf7025501
Greg Doud <gpdoud@gmail.com>
2023-02-15 12:08:01 -0500

    Add project files.

---
+++ geometric-shapes-c39/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace geometric_shapes_c39;
+internal class Person {
+
+    public static string Fullname(string firstname, string lastname) {
+        return $"{firstname} {lastname}";
+    }
+
+}
---
+++ geometric-shapes-c39/Program.cs
@@ -0,0 +1,34 @@
+﻿
+using geometric_shapes_c39;
+
+Sqr s1 = new Sqr(5);
+
+Console.WriteLine($"Perimeter is {s1.Perimeter()} and Area is {s1.Area()}");
+
+Rect r1 = new Rect(3, 5);
+
+Console.WriteLine($"Perimeter is {r1.Perimeter()} and Area is {r1.Area()}");
+
+Quad q1 = new Quad(3, 4, 5, 6);
+
+Console.WriteLine($"Perimeter is {q1.Perimeter()}");
+
+
+
+
+
+
+
+
+
+
+
+
+//Console.WriteLine($"2 plus 3 equals {MathLib.Add(2, 3)}");
+//Console.WriteLine($"2 minus 3 equals {MathLib.Subtract(2, 3)}");
+//Console.WriteLine($"2 times 3 equals {MathLib.Multiple(2, 3)}");
+//Console.WriteLine($"2 divided by 3 equals {MathLib.Divide(2, 0)}");
+
+//var fullname = Person.Fullname("Greg", "Doud");
+
+//Console.WriteLine(fullname);
